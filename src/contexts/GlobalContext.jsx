350da4f0f2be28188c04b60928327b7947cb3e17
antonella1986 <antonella.leo.mail@gmail.com>
2025-04-04 12:31:00 +0200

    remove hardcoded API key from fetch URL in GlobalContext

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -8,7 +8,7 @@ export default GlobalContext;
 export const GlobalProvider = ({ children }) => {
 
     useEffect(() => {
-        fetch("https://developers.themoviedb.org/3/apy_key=9a9ff049c4dbaad0ec878d76120e8689")
+        fetch()
             .then(res => res.json())
             .then(data => setSearch(data))
             .catch(err => console.error(err))
@@ -21,3 +21,5 @@ export const GlobalProvider = ({ children }) => {
         </GlobalContext.Provider>
     );
 }
+
+/* "https://developers.themoviedb.org/3/apy_key=9a9ff049c4dbaad0ec878d76120e8689" */
